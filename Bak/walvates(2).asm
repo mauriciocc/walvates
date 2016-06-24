448e66cf25dcccd8cfe066398f0133e461cd3a7f
Ciço <mauriciocc@outlook.com>
2016-06-23 23:07:13 -0300

    Balance UI

--- Bak/walvates(2).asm
+++ Bak/walvates(2).asm
@@ -12,18 +12,12 @@ BUFFER_SIZE EQU 8192
 
 CaptionText        db   "Open File",0
 
-serial_capture_file db "serial_captured_input.txt",0
 
 
 szFilter db "All files *.*",0,"*.*",0,
             "Text files *.txt",0,"*.txt",0,
             "Assembly files *.asm",0,"*.asm",0,
             "Resource files *.rc",0,"*.rc",0,0
-
-asd db "123,45", 0
-readBuffer DB BUFFER_SIZE DUP(?)
-
-balanceRead BalanceCapture <>
 
 
 
@@ -39,36 +33,32 @@ ProcEvento proc uses edi esi hWin:HWND,uMsg:UINT,wParam:WPARAM,lParam:LPARAM
 		invoke InitializeProductDatabase, addr productsFile, hWin
 		mov productsDatabase, eax	
 	.elseif eax==WM_COMMAND
-		;chr$("text")
-		;invoke MessageBox, hWin, chr$("text"), chr$("text"), MB_OK		
+
 		invoke decode_wparam,wParam				
 		
 		.if edx==BN_CLICKED
-			;mov ponteiro_serial, OpenFileDlg(hWin, 0, addr CaptionText, addr szFilter)
 			
-			; Lê balanca
-			; invoke readUranoValue,addr balanceRead, hWin						
-			;invoke SetDlgItemText, hWin, 1002, addr balanceRead.vlrTotal
-			;mov esi, products_pointer
-			;invoke MessageBox, hWin, addr [esi+(37*type Product)].Product.product_name, NULL, MB_OK
-			invoke ReadEanInput, hWin
-			push eax		
-			invoke AddItemBtnClicked, eax, hWin
-			pop eax
-			invoke mem_free, eax
+			; Item Added		
+			.if eax == AddItemBtn
+				invoke ReadEanInput, hWin
+					push eax					
+						invoke AddItemBtnClicked, eax, hWin
+					pop eax
+				invoke mem_free, eax	
+			.endif
 			
-;			invoke findProductByBarcode, eax, products_pointer
-;			.if eax == -1
-;				mov eax, chr$("Item não encontrado")
-;				MSG_BOX eax, eax				
-;				ret
-;			.endif
-;			mov esi, eax
-;			
-;			invoke toDouble,addr [esi].Product.price
-;			SUM_FP realValue
-			;mov eax, real8$(realValue)
-			;invoke SetDlgItemText, hWin, FTotalValue, eax
+			; Close Sale
+			.if eax == CloseSaleBtn
+				invoke CloseSaleBtnClicked, hWin				
+			.endif
+			
+			; Read Balance
+			.if eax == ReadBalanceBtn
+				invoke ReadBalanceBtnClicked, hWin								
+			.endif
+			
+			
+
 			
 		.endif
 				
@@ -96,4 +86,29 @@ start:
 	invoke DialogBoxParam, Inst_principal, ID_DLG1, NULL, addr ProcEvento, NULL
 	invoke ExitProcess,0
 	;fim do código
-end start	
+end start
+
+
+		;chr$("text")
+		;invoke MessageBox, hWin, chr$("text"), chr$("text"), MB_OK	
+		
+					;mov ponteiro_serial, OpenFileDlg(hWin, 0, addr CaptionText, addr szFilter)
+			
+			; Lê balanca
+			; invoke readUranoValue,addr balanceRead, hWin						
+			;invoke SetDlgItemText, hWin, 1002, addr balanceRead.vlrTotal
+			;mov esi, products_pointer
+			;invoke MessageBox, hWin, addr [esi+(37*type Product)].Product.product_name, NULL, MB_OK
+		
+		;			invoke findProductByBarcode, eax, products_pointer
+;			.if eax == -1
+;				mov eax, chr$("Item não encontrado")
+;				MSG_BOX eax, eax				
+;				ret
+;			.endif
+;			mov esi, eax
+;			
+;			invoke toDouble,addr [esi].Product.price
+;			SUM_FP realValue
+			;mov eax, real8$(realValue)
+			;invoke SetDlgItemText, hWin, FTotalValue, eax		
